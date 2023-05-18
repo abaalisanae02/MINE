af24ae557e17f31beb90502669cbe86723d38621
nohaben <124563332+nohaben@users.noreply.github.com>
2023-05-18 13:59:39 +0200

    Add files via upload

---
+++ register/register.page.spec.ts
@@ -0,0 +1,12 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RegisterPage } from './register.page';
+
+describe('RegisterPage', () => {
+  let component: RegisterPage;
+  let fixture: ComponentFixture<RegisterPage>;
+
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
